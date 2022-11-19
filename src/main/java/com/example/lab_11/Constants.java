097b495b9fd7612725fa7284036e5aff30db99f2
FRMCrozier <113292330+FRMCrozier@users.noreply.github.com>
2022-11-19 22:17:22 +0200

    Delete src/main directory

--- src/main/java/com/example/lab_11/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.lab_11;
-
-public class Constants {
-    static final String SERVER = "localhost:3306";
-    static final String DATABASE = "test";
-    static final String URL = "jdbc:mysql://" + SERVER + "/" + DATABASE;
-}
