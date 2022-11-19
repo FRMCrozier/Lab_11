cae3d3db8b5615a42bc90fd756812465b7cdb3ac
FRMCrozier <113292330+FRMCrozier@users.noreply.github.com>
2022-11-19 22:18:34 +0200

    Initial commit

---
+++ lab_11/src/main/java/com/example/lab_11/Constants.java
@@ -0,0 +1,7 @@
+package com.example.lab_11;
+
+public class Constants {
+    static final String SERVER = "localhost:3306";
+    static final String DATABASE = "test";
+    static final String URL = "jdbc:mysql://" + SERVER + "/" + DATABASE;
+}
